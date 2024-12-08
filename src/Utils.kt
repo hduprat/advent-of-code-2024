cedf85893d47d1486b4a14dfb483e5789d043848
hduprat <hugod@bam.tech>
2024-12-08 12:02:15 +0100

    Day 8

--- src/Utils.kt
+++ src/Utils.kt
@@ -26,6 +26,10 @@ fun Any?.println() = println(this)
 fun <T> List<T>.isSortedWith(comparator: Comparator<T>): Boolean =
     this.windowed(2).all { (a, b) -> comparator.compare(a, b) < 0 }
 
+fun <T> List<T>.combinations(): List<Pair<T, T>> = this.flatMapIndexed { i, elt ->
+    this.slice(i + 1 until this.size).map { elt to it }
+}
+
 val <T> List<T>.middle get() = this[this.size / 2]
 
 /*
@@ -37,4 +41,10 @@ fun Long.concat(l: Long): Long = "$this$l".toLong()
  * IntVector
  */
 typealias IntVector = Pair<Int, Int>
-operator fun IntVector.plus(other: IntVector) = this.first + other.first to this.second + other.second
+
+operator fun IntVector.plus(other: IntVector): IntVector = this.first + other.first to this.second + other.second
+operator fun IntVector.minus(other: IntVector): IntVector = this.first - other.first to this.second - other.second
+operator fun IntVector.times(n: Int): IntVector = n * this.first to n * this.second
+operator fun Int.times(vec: IntVector): IntVector = vec.times(this)
+
+fun IntVector.inRect(height: Int, width: Int) = this.first in 0 until width && this.second in 0 until height
