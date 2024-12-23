7348e4d6b5a9332b37d3323432acaa4b0d01630c
hduprat <hugod@bam.tech>
2024-12-23 16:21:05 +0100

    Day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,114 +1,90 @@
-class GuardMap(input: List<String>) {
-    private val height = input.size
-    private val width = input[0].length
-    private val obstacles: List<IntVector>
-    private var guardDirection: Direction = Direction.UP
-    private val initialDirection: Direction
-    private var guardPosition: IntVector = 0 to 0
-    val initialPosition: IntVector
-    private val allDirections = mutableListOf<Pair<IntVector, Direction>>()
+class Lab(
+    private val height: Int,
+    private val width: Int,
+    private val obstacles: List<IntVector>,
+    private val guardPosition: IntVector
+) {
 
-    val allPositions = mutableListOf<IntVector>()
-    var extraObstacle: IntVector? = null
-
-    init {
-        val tempObstacles = mutableListOf<IntVector>()
-
-        input.forEachIndexed { y, line ->
-            line.forEachIndexed { x, c ->
-                when (c) {
-                    '#' -> tempObstacles.add(x to y)
-                    '^' -> {
-                        guardDirection = Direction.UP
-                        guardPosition = x to y
-                    }
-
-                    '>' -> {
-                        guardDirection = Direction.RIGHT
-                        guardPosition = x to y
-                    }
-
-                    'v' -> {
-                        guardDirection = Direction.DOWN
-                        guardPosition = x to y
-                    }
-
-                    '<' -> {
-                        guardDirection = Direction.LEFT
-                        guardPosition = x to y
-                    }
-                }
+    constructor(input: List<String>) : this(input.size, input[0].length, buildList {
+        parseGrid(input) { point, c -> if (c == '#') add(point) }
+    }, input.run {
+        lateinit var result: IntVector
+        parseGrid(this) { point, c ->
+            if (c == '^') {
+                result = point
             }
         }
+        result
+    })
 
-        obstacles = tempObstacles.toList()
-        initialDirection = guardDirection
-        initialPosition = guardPosition.copy()
-    }
+    fun getGuardRound(): List<IntVector> {
+        var position = guardPosition
+        var direction = Direction.UP
 
-    fun reset() {
-        guardPosition = initialPosition.copy()
-        guardDirection = initialDirection
-        allPositions.clear()
-        allPositions.add(initialPosition)
-        allDirections.clear()
-    }
+        val positions = mutableListOf<Pair<IntVector, Direction>>()
 
-    val isGuardInMap get() = guardPosition.first in 0 until width && guardPosition.second in 0 until height
+        do {
+            positions += position to direction
 
-    fun advanceGuard() {
-        allPositions.add(guardPosition)
-        var newPosition = guardPosition + guardDirection.vector
-        if (newPosition in obstacles || newPosition == extraObstacle) {
-            guardDirection = Direction.entries[(guardDirection.ordinal + 1) % Direction.entries.size]
-
-            if (guardPosition to guardDirection in allDirections) {
-                throw Exception("In a loop")
+            val nextPos = position + direction.vector
+            if (nextPos to direction in positions) throw IllegalStateException("Loop encountered")
+            if (nextPos in obstacles) {
+                direction = direction.next
+                position += direction.vector
+            } else {
+                position = nextPos
             }
-            allDirections.add(guardPosition to guardDirection)
-            newPosition = guardPosition + guardDirection.vector
-        }
-        guardPosition = newPosition
+        } while (position.inRect(height, width))
+
+
+
+        return positions.map { it.first }
     }
+
+    fun isInLoop(): Boolean {
+        var position = guardPosition
+        var direction = Direction.UP
+
+        val positions = mutableListOf<Pair<IntVector, Direction>>()
+
+        do {
+
+            val nextPos = position + direction.vector
+            if (nextPos to direction in positions) return true
+            if (nextPos in obstacles) {
+                positions += position to direction
+                direction = direction.next
+            } else {
+                position = nextPos
+            }
+        } while (position.inRect(height, width))
+
+        return false
+    }
+
+    fun withExtraObstacle(obstacle: IntVector): Lab = Lab(height, width, obstacles + obstacle, guardPosition)
 }
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val map = GuardMap(input)
-        do {
-            map.advanceGuard()
-        } while (map.isGuardInMap)
-        return map.allPositions.distinct().size
+        val lab = Lab(input)
+
+        return lab.getGuardRound().distinct().size
     }
 
-    /*
-     * I don't know why my result is incorrect here. I might have to just draw a schema and then retry part 2 in its entirety.
-     */
     fun part2(input: List<String>): Int {
-        val map = GuardMap(input)
+        val lab = Lab(input)
+        val guardRound = lab.getGuardRound()
 
-        do {
-            map.advanceGuard()
-        } while (map.isGuardInMap)
-        map.advanceGuard()
-
-        val positions = map.allPositions.distinct().filter { it != map.initialPosition }
-
-        val configurations = mutableSetOf<IntVector>()
-        for (point in positions) {
-            map.reset()
-            map.extraObstacle = point
-            try {
-                do {
-                    map.advanceGuard()
-                } while (map.isGuardInMap)
-            } catch (exc: Exception) {
-                println("Placing an obstacle in ${map.extraObstacle} puts the guard in a loop.")
-                configurations.add(point)
-            }
+        val extraObstaclePotentialPositions = buildList {
+            addAll(guardRound.distinct())
+            removeAt(0)
         }
 
-        return configurations.size
+        return extraObstaclePotentialPositions.map {
+            println("Simulating obstacle at $it.")
+            lab.withExtraObstacle(it).isInLoop()
+        }.count { it }
     }
 
     val testInput = readInput("Day06_test")
@@ -117,6 +93,6 @@ fun main() {
     check(part1(testInput) == 41)
     part1(input).println()
 
-//    check(part2(testInput) == 6)
+    check(part2(testInput) == 6)
     part2(input).println()
 }
